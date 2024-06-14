81ff47493ce0df947c8fda155bfa68c69217e152
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-06-14 15:53:51 +0200

    use matrix operator, reduce to even sector

--- braiding_extra_majoranas.jl
+++ braiding_extra_majoranas.jl
@@ -11,15 +11,23 @@ majorana_labels = 0:5
 
 ## Couplings
 P = parity_operators(γ)
+P = Dict(map((kp) -> kp[1] => kp[2][2^2+1:end, 2^2+1:end], collect(P))); #Only take the even sector
 Ps = (P[0, 1], P[0, 2], P[0, 3]);
 ## 
 function H((T, Δmin, Δmax, k), t)
     Δs = braiding_deltas(t, T, Δmin, Δmax, k)
     sum(Δ * P for (Δ, P) in zip(Δs, Ps))
 end
+function mat_update!(A, u, (T, Δmin, Δmax, k), t)
+    fill!(A, 0)
+    for (Δ, P) in zip(braiding_deltas(t, T, Δmin, Δmax, k), Ps)
+        A .+= -1im * Δ * P
+    end
+end
+M = MatrixOperator(rand(ComplexF64, size(first(P)[2])...); (update_func!)=mat_update!)
 
 ## Parameters
-u0 = collect(first(eachcol(eigen(Hermitian(P[2, 4] + P[3, 5] - P[0, 1]), 1:1).vectors)))
+u0 = collect(first(eachcol(eigen(Hermitian(P[2, 4] + P[3, 5] + P[0, 1]), 1:1).vectors)))
 T = 1 # Total time for single braiding
 k = 50 / T # Larger k means steeper steps for the couplings
 Δmax = 1e3 / T
@@ -27,13 +35,14 @@ k = 50 / T # Larger k means steeper steps for the couplings
 p = (T, Δmin, Δmax, k)
 tspan = (0.0, 2T)
 ##
-prob = ODEProblem(drho!, u0, tspan, p)
+prob = ODEProblem(M, u0, tspan, p)
 ts = range(0, tspan[2], 300)
 deltas = stack([braiding_deltas(t, p...) for t in ts])'
 plot(ts, deltas, label=["Δ01" "Δ02" "Δ03"], xlabel="t", ls=[:solid :dash :dot], lw=3)
 
 ## Solve the system
-@time sol = solve(prob, Tsit5(), saveat=ts, reltol=1e-12, tstops=ts);
+@time sol = solve(prob, Tsit5(), saveat=ts, abstol=1e-6, reltol=1e-6, tstops=ts);
+#@time sol = solve(prob, LinearExponential(), saveat=ts, abstol=1e-6, reltol=1e-6, dt=1e-4);
 plot(ts, [1 .- norm(sol(t)) for t in ts], label="norm error", xlabel="t")
 
 ##
@@ -44,5 +53,5 @@ plot(plot(ts, [real(sol(t)'m * sol(t)) for m in measurements, t in ts]', label=p
 
 ##
 prob_full = ODEProblem(drho!, Matrix{ComplexF64}(I, size(u0, 1), size(u0, 1)), tspan, p)
-@time sol_full = solve(prob_full, Tsit5(), saveat=ts, reltol=1e-12, tstops=ts);
+@time sol_full = solve(prob_full, Tsit5(), saveat=ts, abstol=1e-6, reltol=1e-6, tstops=ts);
 sol_full(2T)
