46c3e74bdcb06371438642aeb17d3a4a880c12d1
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-06-14 10:48:00 +0200

    change step function, change majorana-fermion combinations

--- braiding_extra_majoranas.jl
+++ braiding_extra_majoranas.jl
@@ -12,14 +12,14 @@ majorana_labels = 0:5
 ## Couplings
 P = parity_operators(γ)
 Ps = (P[0, 1], P[0, 2], P[0, 3]);
-##
+## 
 function H((T, Δmin, Δmax, k), t)
     Δs = braiding_deltas(t, T, Δmin, Δmax, k)
     sum(Δ * P for (Δ, P) in zip(Δs, Ps))
 end
 
 ## Parameters
-u0 = first(eachcol(eigen(Hermitian(P[1, 4] + P[2, 5] - P[0, 3]), 1:1).vectors))
+u0 = collect(first(eachcol(eigen(Hermitian(P[1, 4] + P[2, 5] - P[0, 3]), 1:1).vectors)))
 T = 1 # Total time for single braiding
 k = 50 / T # Larger k means steeper steps for the couplings
 Δmax = 1e3 / T
@@ -37,7 +37,7 @@ plot(ts, deltas, label=["Δ01" "Δ02" "Δ03"], xlabel="t", ls=[:solid :dash :dot
 plot(ts, [1 .- norm(sol(t)) for t in ts], label="norm error", xlabel="t")
 
 ##
-parities = [(1, 4), (2, 5), (0, 3)]
+parities = [(2, 3), (2, 4), (3, 5)]
 measurements = map(p -> P[p...], parities)
 plot(plot(ts, [real(sol(t)'m * sol(t)) for m in measurements, t in ts]', label=permutedims(parities), legend=true, xlabel="t"),
     plot(ts, deltas, label=["Δ01" "Δ02" "Δ03"], xlabel="t", ls=[:solid :dash :dot], lw=1), layout=(2, 1), lw=2, frame=:box)
--- misc.jl
+++ misc.jl
@@ -4,15 +4,19 @@ struct MajoranaWrapper{B,D}
     function MajoranaWrapper(basis, labels=collect(Base.product(keys(basis), (:a, :b))))
         N = length(basis)
         length(labels) == 2 * N || throw(ErrorException("Number of majoranas is not twice the fermion number"))
-        majA = map(f -> f + f', basis)
-        majB = map(f -> 1im * (f - f'), basis)
-        dA = QuantumDots.dictionary(zip(labels[1:N], values(majA)))
-        dB = QuantumDots.dictionary(zip(labels[N+1:2N], values(majB)))
-        d = merge(dA, dB)
+        majs = vec(reduce(hcat, [[f + f', 1im * (f - f')] for f in basis]))
+        d = QuantumDots.dictionary(zip(labels, values(majs)))
         new{typeof(basis),typeof(d)}(basis, d)
     end
 end
 Base.getindex(g::MajoranaWrapper, i...) = g.majoranas[i...]
+
+function MajoranaBasis(labels; qn=QuantumDots.parity)
+    N = length(labels)
+    iseven(N) || throw(ErrorException("Number of majoranas must be even"))
+    c = FermionBasis(1:div(N, 2); qn)
+    MajoranaWrapper(c, labels)
+end
 
 smooth_step(x, k) = 1 / 2 + tanh(k * x) / 2
 
@@ -29,12 +33,11 @@ end
 ## Give the value of the three deltas at time t in the three point majorana braiding protocol
 function braiding_deltas(t, T, Δmin, Δmax, k, args...)
     Δ1 = Δtrajectory(t, T, Δmin, Δmax, k)
-    Δ2 = Δtrajectory(t + T / 3, T, Δmin, Δmax, k)
-    Δ3 = Δtrajectory(t + 2T / 3, T, Δmin, Δmax, k)
+    Δ2 = Δtrajectory(t - T / 3, T, Δmin, Δmax, k)
+    Δ3 = Δtrajectory(t - 2T / 3, T, Δmin, Δmax, k)
     return Δ1, Δ2, Δ3
 end
 function Δtrajectory(t, T, Δmin, Δmax, k, args...)
     dΔ = Δmax - Δmin
-    tp = mod(t, T) - 3T / 12
-    Δmin + dΔ * smooth_step(tp - 1T / 6, k) - dΔ * smooth_step(tp - 4T / 6, k)
+    Δmin + dΔ * smooth_step(cos(2pi * t / T), k)
 end
