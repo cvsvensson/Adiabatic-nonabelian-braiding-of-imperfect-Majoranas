f9f93daa47a5610248fb0405617d9c00fdcffc14
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-06-24 12:59:43 +0200

    faster hamiltonian

--- braiding_extra_majoranas.jl
+++ braiding_extra_majoranas.jl
@@ -14,14 +14,14 @@ P = parity_operators(γ)
 P = Dict(map((kp) -> kp[1] => kp[2][2^2+1:end, 2^2+1:end], collect(P))); #Only take the even sector
 Ps = (P[0, 1], P[0, 2], P[0, 3]);
 ## 
-function H((T, Δmin, Δmax, k), t)
+function H((T, Δmin, Δmax, k, Ps), t)
     Δs = braiding_deltas(t, T, Δmin, Δmax, k)
     sum(Δ * P for (Δ, P) in zip(Δs, Ps))
 end
-function mat_update!(A, u, (T, Δmin, Δmax, k), t)
+function mat_update!(A, u, (T, Δmin, Δmax, k, Ps), t)
     fill!(A, 0)
     for (Δ, P) in zip(braiding_deltas(t, T, Δmin, Δmax, k), Ps)
-        A .+= -1im * Δ * P
+        A .+= -1im .* Δ .* P
     end
 end
 M = MatrixOperator(rand(ComplexF64, size(first(P)[2])...); (update_func!)=mat_update!)
@@ -32,7 +32,7 @@ T = 1 # Total time for single braiding
 k = 50 / T # Larger k means steeper steps for the couplings
 Δmax = 1e3 / T
 Δmin = 1e-3 * 1 / T
-p = (T, Δmin, Δmax, k)
+p = (T, Δmin, Δmax, k, Ps)
 tspan = (0.0, 2T)
 ##
 prob = ODEProblem(M, u0, tspan, p)
