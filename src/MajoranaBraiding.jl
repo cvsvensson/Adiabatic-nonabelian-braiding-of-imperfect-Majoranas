876a6684852065b25094356c198495f27110daea
Viktor Svensson <svensson.viktor.91@gmail.com>
2024-06-25 12:37:33 +0200

    start making a package

---
+++ src/MajoranaBraiding.jl
@@ -0,0 +1,5 @@
+module MajoranaBraiding
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MajoranaBraiding
+using Test
+
+@testset "MajoranaBraiding.jl" begin
+    # Write your tests here.
+end
