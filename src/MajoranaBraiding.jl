b2c3389b53a9e55e22eeef33f236886cf1ba0924
Viktor Svensson <svensson.viktor.91@gmail.com>
2025-07-10 10:46:04 +0200

    clean up exports

--- src/MajoranaBraiding.jl
+++ src/MajoranaBraiding.jl
@@ -12,11 +12,11 @@ using FermionicHilbertSpaces
 
 export parity_operators
 export majorana_exchange, gate_fidelity, analytical_gate_fidelity
-export SimpleCorrection, IndependentSimpleCorrection, InterpolatedExactSimpleCorrection, NoCorrection
+export SimpleCorrection, InterpolatedExactSimpleCorrection, NoCorrection, OptimizedSimpleCorrection, IndependentSimpleCorrection, OptimizedIndependentSimpleCorrection
 export single_braid_gate_kato, single_braid_gate_analytical
 export setup_problem, solve
 export visualize_protocol, visualize_parities, visualize_analytic_parameters, visualize_spectrum, visualize_rhos, visualize_deltas
-export diagonal_majoranas, OptimizedSimpleCorrection, IndependentSimpleCorrection, OptimizedIndependentSimpleCorrection
+export diagonal_majoranas
 
 include("hamiltonians.jl")
 include("analytic_correction.jl")
